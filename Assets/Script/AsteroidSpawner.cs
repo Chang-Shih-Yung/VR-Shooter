4b120cba3bd4f8849f38a14f5665dd057207ff81
Chang-Shih-Yung <henry1010921@gmail.com>
2024-05-13 16:58:07 +0800

    完成生成小行星腳本

---
+++ Assets/Script/AsteroidSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    //給標題
+    [Header("Size of spawner area")]
+    public Vector3 spawnerSize;
+
+    [Header("Rate of spawn")]
+    public float spawnRate = 1f;
+    [Header("Model to spawn")]
+    [SerializeField] private GameObject asteroidModel;
+    private float spawnTimer = 0f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, spawnerSize);
+    }
+
+
+    //生成物體計時器
+    private void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnRate)
+        {
+            Debug.Log("Spawning");
+            spawnTimer = 0;
+            SpawnAsteroid();
+        }
+    }
+
+    //小行星生成方法
+    private void SpawnAsteroid()
+    {
+        //盒子大小內的隨機位置:X>>>>-10~10=20,y>>>-10~10=20,z>>>-20~20=40
+        //將物件原本座標賦予隨機位置:實例化非靜態聲明新的位置
+        Vector3 spawnPoint = transform.position + new Vector3(UnityEngine.Random.Range(-spawnerSize.x / 2, spawnerSize.x / 2),
+        UnityEngine.Random.Range(-spawnerSize.y / 2, spawnerSize.y / 2),
+        UnityEngine.Random.Range(-spawnerSize.z / 2, spawnerSize.z / 2));
+        GameObject asteroid = Instantiate(asteroidModel, spawnPoint, transform.rotation);
+        //將生成的小行星放到此腳本的遊戲物件內，避免生成一大堆編輯器會很亂
+        asteroid.transform.SetParent(this.transform);
+    }
+}
+
