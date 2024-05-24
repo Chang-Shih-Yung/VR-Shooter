4439bcf27d59284661c5f07ce9e769463d7a7760
Chang-Shih-Yung <henry1010921@gmail.com>
2024-05-24 18:32:27 +0800

    雷射槍腳本功能新增

--- Assets/Script/LaserGun.cs
+++ Assets/Script/LaserGun.cs
@@ -2,10 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class LaserGun : MonoBehaviour
 {
+
+    //動畫
+    [SerializeField] private Animator laserAnimator;
+    //音效
+    [SerializeField] private AudioClip laserSFX;
+
+    private AudioSource laserAudioSource;
+
+    //start之前觸發，觸發後再start:抓組件
+    private void Awake()
+    {
+        laserAudioSource = GetComponent<AudioSource>();
+    }
+    //射線
     public void LeserGunFired()
     {
+        //播放動畫
+        laserAnimator.SetTrigger("Fire");
+        //播放音效
+        laserAudioSource.PlayOneShot(laserSFX);
+        //射線
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
